af2cf39902a840e2ba954c4e373c8205b5a0e15b
Mostafa Hisham <52044516+mostafahisham03@users.noreply.github.com>
2023-12-11 01:27:10 +0200

    add upcoming deadline

---
+++ Advising_Team/Advising_Team/Student/Upcoming_Installment.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="GUC Advising System - Upcoming Installment" Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="Upcoming_Installment.aspx.cs" Inherits="Advising_Team.Student.Upcoming_Installment" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>GUC Advising System - Upcoming Installment</h2><br />
+    <asp:Label ID="Date" runat="server" Text="" Visible="false"></asp:Label>
+</asp:Content>
---
+++ Advising_Team/Advising_Team/Student/Upcoming_Installment.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Advising_Team.Student
+{
+    public partial class Upcoming_Installment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["Advising_System"].ToString();
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                // redirect if the session is closed
+                if (Session["user"] == null)
+                {
+                    Response.Redirect("Student_Login.aspx");
+                    return;
+                }
+                using (SqlCommand upcomProc = new SqlCommand("dbo.[FN_StudentUpcoming_installment]", conn))
+                {
+                    int studentId = (int)Session["user"];
+                    upcomProc.CommandType = CommandType.StoredProcedure;
+                    upcomProc.Parameters.Add(new SqlParameter("@student_ID", studentId));
+
+                    SqlParameter installdeadline = new SqlParameter("@installdeadline", SqlDbType.Date);
+                    installdeadline.Direction = ParameterDirection.ReturnValue;
+                    upcomProc.Parameters.Add(installdeadline);
+
+                    upcomProc.ExecuteNonQuery();
+                    DateTime x = Convert.ToDateTime(installdeadline.Value); 
+                    Date.Text = x.ToString();
+                    Date.Visible = true;
+
+                }
+            }
+        }
+    }
+}
